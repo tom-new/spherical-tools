27be25da0502eda05899b33418b187cc07387db9
tom-new <tomnewtonnew@gmail.com>
2025-07-23 13:31:48 +1000

    add python files

---
+++ src/spherical_tools/__init__.py
@@ -0,0 +1,17 @@
+from .spherical import (
+    cart2sph,
+    geo2sph,
+    sph2geo,
+    geo2sph,
+    cart2polar,
+    great_circle_distance,
+)
+
+__all__ = [
+    "cart2sph",
+    "geo2sph",
+    "sph2geo",
+    "geo2sph",
+    "cart2polar",
+    "great_circle_distance",
+]
